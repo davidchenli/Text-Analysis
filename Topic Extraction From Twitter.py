403292e2104f3da93360b55a2fba91acdf288097
David <86958528+davidchenli@users.noreply.github.com>
2021-07-11 23:38:00 +0800

    Updated

--- Topic Extraction From Twitter.py
+++ Topic Extraction From Twitter.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
 import os
 import pandas as pd
 import numpy as np
@@ -20,8 +14,6 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import NMF
 from numpy.linalg import svd
 
-
-# In[10]:
 
 
 def corpus( path, encoding='utf8', nonalphabetic=False, tolower=False ):
@@ -43,15 +35,10 @@ def corpus( path, encoding='utf8', nonalphabetic=False, tolower=False ):
     return doc8raw, filenames
 
 
-# In[11]:
-
 
 os.chdir(f"C:/Users/USER/Desktop/spritzer15/spritzer/15")
 a = os.listdir(f"C:/Users/USER/Desktop/spritzer15/spritzer/15")
 text,name=corpus(a,encoding = 'latin1')
-
-
-# In[12]:
 
 
 clean = []
@@ -61,13 +48,7 @@ for texts in text:
     clean.append(texts)
 
 
-# In[13]:
-
-
 stop = {'tag','url','co', 'amp', 'via', 'don', 'dont'}|set(stopwords.words("english"))|set(stopwords.words("spanish"))|set(stopwords.words("portuguese"))
-
-
-# In[14]:
 
 
 normal=CountVectorizer(stop_words=stop)
@@ -84,17 +65,11 @@ print("Drop document with no element: %d rows were dropped"%sum(dt.sum(1)==0))
 dt=dt[dt.sum(1)!=0,:]
 
 
-# In[15]:
-
-
 ### NMF Method without tfidf
 r = 10
 model = NMF(n_components=r, init='random', random_state=0,max_iter=1000)
 W = model.fit_transform(dt)
 H = model.components_
-
-
-# In[16]:
 
 
 topic=[]
@@ -103,9 +78,6 @@ for k in range(10):
     topic = {k: v for k, v in sorted(topic.items(), key=lambda item: item[1], reverse=True)} # discard stop words and sort by value
     topicwords = list(topic.keys())
     print("\nTopic ", k, ": \t", topicwords[0 : 10])
-
-
-# In[17]:
 
 
 ### SVD without TFIDF
@@ -120,19 +92,12 @@ docproj = Uk.dot(Dk)
 termproj = Dk.dot(Vk).T
 
 
-
-# In[18]:
-
-
 topic=[]
 for k in range(10):
     topic = dict(zip(terms, termproj[:,k]))
     topic = {k: v for k, v in sorted(topic.items(), key=lambda item: item[1], reverse=True)} # discard stop words and sort by value
     topicwords = list(topic.keys())
     print("\nTopic ", k, ": \t", topicwords[0 : 10])
-
-
-# In[19]:
 
 
 ### LDA without TFIDF
@@ -149,9 +114,6 @@ for k in range(0, T):
     print("\nTopic ", k, ": \t", topicwords[0 : 10])
 
 
-# In[20]:
-
-
 # Final Graph
 topic = dict(zip(terms, topicterms[4,:]))
 topic = {k: v for k, v in sorted(topic.items(), key=lambda item: item[1], reverse=True)}
@@ -160,18 +122,11 @@ topc_10=(pd.DataFrame(topic.values(),index=topic.keys())).iloc[0:10,:]
 wcloud = WordCloud().generate_from_frequencies( dict(zip(topc_10.index,topc_10.iloc[:,0])) )
 plt.imshow(wcloud, interpolation='bilinear')
 
-
-# In[21]:
-
-
 ############ Additional try: tfidf method
 
 f=dt/(np.reshape(dt.sum(1), (-1,1)).dot( np.ones((1,dt.shape[1])) ))
 idf=np.log2(len((dt>0)[1,])/((dt>0).sum(0)+0.1) )
 weighted=f*idf
-
-
-# In[22]:
 
 
 ######### NMF with tfidf method
@@ -186,8 +141,6 @@ for k in range(10):
     topicwords = list(topic.keys())
     print("\nTopic ", k, ": \t", topicwords[0 : 10])
 
-
-# In[23]:
 
 
 ######### SVD with tfidf
